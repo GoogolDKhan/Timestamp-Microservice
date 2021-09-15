1e0297f55f261e0840fcfeb962eaa7e282bff169
Sarfaraz <khansarfaraz2141@gmail.com>
2021-09-15 12:51:09 +0530

    Solution to the project

--- server.js
+++ server.js
@@ -24,7 +24,46 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+let resObj = {};
 
+app.get("/api/:input", (req, res) => {
+  let input = req.params.input;
+
+  if (/\d{5,}/.test(input)) {
+    /* timestamp 1451001600000*/
+    input = parseInt(input);
+    resObj["unix"] = new Date(input).getTime();
+    resObj["utc"] = new Date(input).toUTCString();
+  } else {
+    /* Date String 2015-12-25, 25 December 2015 */
+    resObj["unix"] = new Date(input).getTime();
+    resObj["utc"] = new Date(input).toUTCString();
+  }
+
+  if (!resObj["unix"] || !resObj["utc"]) {
+    res.json({ error: "Invalid Date" });
+  }
+
+  res.json(resObj);
+});
+
+app.get("/api", (req, res) => {
+  resObj["unix"] = new Date().getTime();
+  resObj["utc"] = new Date().toUTCString();
+
+  res.json(resObj);
+});
+
+// BODY PARSER
+
+let bodyParser = require('body-parser')
+
+app.post('/api/', bodyParser.urlencoded({ extended: false }), (req, res) => {
+  let input = req.body.input
+  let getUrl = '/api/' + input
+
+  res.redirect(getUrl)
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
