10eb7d1e1c7db5c3ae34a907d9468abbfe2deddc
Sarfaraz <khansarfaraz2141@gmail.com>
2021-10-01 00:26:09 +0530

    feat: Listen on port 3000

--- server.js
+++ server.js
@@ -65,6 +65,6 @@ app.post("/api/", bodyParser.urlencoded({ extended: false }), (req, res) => {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
